9487b44257af935d9ede691a5ed71b2f8a1d63de
MIXERRY4X <malikkanphairee123@gmail.com>
2026-02-05 15:22:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-body{
-  background:#0f172a;
-  color:white;
-  font-family:Arial;
-  text-align:center;
-  padding-top:100px;
-}
-
-input{
-  padding:10px;
-}
-
-button{
-  padding:10px;
-  background:green;
-  color:white;
-  border:none;
-}
-
 function checkPass(){
 
-  var password = document.getElementById("pass").value;
+  var p = document.getElementById("pass").value;
 
-  if(password === "1234"){
-    document.getElementById("msg").innerHTML = "รหัสถูกต้อง";
-    window.location.href = "home.html"; 
+  if(p === "FHM4X"){
+    window.location.href = "home.html";
   } 
   else{
     document.getElementById("msg").innerHTML = "รหัสผิด";
